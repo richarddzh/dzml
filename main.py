e51fefbff9edbdd619aa8b00347b0a1fe2b1a903
Richard Dong <richarddzh@163.com>
2018-04-26 21:41:38 +0800

    Learn to use range_input_producer

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from mmml import helpers
+import mmml.hello as hello
 
 
-text = "richard 中文"
-char_map = helpers.file_read_all_text(r'dataset\ZiDian.txt', 'utf-8')
-a = helpers.map_text_to_integers(text, char_map)
-print(a)
-
-a = helpers.file_read_text_as_integers(r'dataset\NuoWeiDeSenLin.txt', r'dataset\ZiDian.txt', 'utf-8')
-print(a[0:20])
-a = helpers.file_read_all_text(r'dataset\NuoWeiDeSenLin.txt', 'utf-8')
-print(a[0:20])
+hello.Hello.use_range_input_producer()
--- mmml/hello.py
+++ mmml/hello.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+
+
 class Hello:
     def __init__(self, name):
         self.name = name
@@ -8,3 +11,19 @@ class Hello:
 
     def say_hi(self):
         Hello.say(self.name)
+
+    @staticmethod
+    def use_range_input_producer():
+        with tf.Graph().as_default():
+            i = tf.train.range_input_producer(4).dequeue()
+            # Usage 1
+            with tf.train.MonitoredTrainingSession() as session:
+                for j in range(10):
+                    a = session.run(i)
+                    print(a)
+            # Usage 2
+            sv = tf.train.Supervisor()
+            with sv.managed_session() as session:
+                for j in range(10):
+                    a = session.run(i)
+                    print(a)
