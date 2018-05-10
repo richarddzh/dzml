8ecf38d4f216245b2b8a8836518cf8ace5dc4bdd
Richard Dong <richarddzh@163.com>
2018-05-10 11:23:25 +0000

    Read text with sequence input producer

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import mmml.helpers as helpers
 import tensorflow as tf
 
 
-char_map = helpers.file_read_all_text(r'dataset\ZiDian.txt', 'utf-8')
-text = helpers.file_read_all_text(r'dataset\BaiYeXing.txt', 'utf-8')
+char_map = helpers.file_read_all_text(r'dataset\dict.txt', 'utf-8')
+text = helpers.file_read_all_text(r'dataset\baiyexing.txt', 'utf-8')
 data = helpers.map_text_to_integers(text, char_map)
 batch_size = 2
 num_step = 4
 
 with tf.Graph().as_default():
     x, y = helpers.sequence_input_producer(data, batch_size, num_step)
+    sv = tf.train.Supervisor()
+    with sv.managed_session() as session:
+        for i in range(2):
+            print(i)
+            a = session.run((x, y))
+            print(a)
 
